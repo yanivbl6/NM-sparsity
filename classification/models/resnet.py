4b69bbc907515aa6a4457365eeaa907af9548c38
Yaniv Blumenfeld <yanivblm6@gmail.com>
2021-05-05 14:41:12 +0300

    added N:P:M sparsity, v0

--- classification/models/resnet.py
+++ classification/models/resnet.py
@@ -18,10 +18,10 @@ __all__ = ['ResNetV1', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
 
 
 
-def conv3x3(in_planes, out_planes, stride=1, N=2, M=4):
+def conv3x3(in_planes, out_planes, stride=1, N=2, P=0, M=4):
     """3x3 convolution with padding"""
     return SparseConv(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False, N=N, M=M)
+                     padding=1, bias=False, N=N, P=P, M=M)
 
 
 class BasicBlock(nn.Module):
@@ -30,10 +30,10 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None, N=2, M=4):
         super(BasicBlock, self).__init__()
 
-        self.conv1 = conv3x3(inplanes, planes, stride, N=N, M=M)
+        self.conv1 = conv3x3(inplanes, planes, stride, N=N, P=, M=M)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes, N=N, M=M)
+        self.conv2 = conv3x3(planes, planes, N=N, P=, M=M)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
@@ -59,15 +59,15 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, N=2, M=4):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, N=2, P =0, M=4):
         super(Bottleneck, self).__init__()
 
-        self.conv1 = SparseConv(inplanes, planes, kernel_size=1, bias=False, N=N, M=M)
+        self.conv1 = SparseConv(inplanes, planes, kernel_size=1, bias=False, N=N, P=P, M=M)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = SparseConv(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False, N=N, M=M)
+                               padding=1, bias=False, N=N, P=P, M=M)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = SparseConv(planes, planes * 4, kernel_size=1, bias=False, N=N, M=M)
+        self.conv3 = SparseConv(planes, planes * 4, kernel_size=1, bias=False, N=N, P=P, M=M)
         self.bn3 = nn.BatchNorm2d(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -97,23 +97,24 @@ class Bottleneck(nn.Module):
 
 class ResNetV1(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, N=2, M=4):
+    def __init__(self, block, layers, num_classes=1000, N=2, P=0, M=4):
         super(ResNetV1, self).__init__()
 
 
         self.N = N
         self.M = M
+        self.P = P
 
         self.inplanes = 64
         self.conv1 = SparseConv(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False, N=self.N, M=self.M)
+                               bias=False, N=self.N, P=self.P , M=self.M)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], N = self.N, M = self.M)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, N = self.N, M = self.M)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, N = self.N, M = self.M)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, N = self.N, M = self.M)
+        self.layer1 = self._make_layer(block, 64, layers[0], N = self.N, P=self.P, M = self.M)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, N = self.N, P=self.P, M = self.M)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, N = self.N, P=self.P, M = self.M)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, N = self.N, P=self.P, M = self.M)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -122,20 +123,20 @@ class ResNetV1(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
 
-    def _make_layer(self, block, planes, blocks, stride=1, N = 2, M = 4):
+    def _make_layer(self, block, planes, blocks, stride=1, N = 2, P=0, M = 4):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 SparseConv(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False,  N=N, M=M),
+                          kernel_size=1, stride=stride, bias=False,  N=N, P=P, M=M),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, N=N, M=M))
+        layers.append(block(self.inplanes, planes, stride, downsample, N=N, P=P, M=M))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, N=N, M=M))
+            layers.append(block(self.inplanes, planes, N=N, P=P, M=M))
 
         return nn.Sequential(*layers)
 
--- devkit/sparse_ops/sparse_ops.py
+++ devkit/sparse_ops/sparse_ops.py
@@ -10,7 +10,7 @@ class Sparse(autograd.Function):
     """" Prune the unimprotant weight for the forwards phase but pass the gradient to dense weight using SR-STE in the backwards phase"""
 
     @staticmethod
-    def forward(ctx, weight, N, M, decay = 0.0002):
+    def forward(ctx, weight, N, P, M, decay = 0.0002):
         ctx.save_for_backward(weight)
 
         output = weight.clone()
@@ -33,11 +33,16 @@ class Sparse(autograd.Function):
 
         weight, = ctx.saved_tensors
         return grad_output + ctx.decay * (1-ctx.mask) * weight, None, None
+
+
+def round_to_power_of_2(w):
+    return torch.pow(2.0, (w.abs().log()/np.log(2.0)).floor()) * w.sign()
+
 class Sparse_NHWC(autograd.Function):
     """" Prune the unimprotant edges for the forwards phase but pass the gradient to dense weight using STE in the backwards phase"""
 
     @staticmethod
-    def forward(ctx, weight, N, M, decay = 0.0002):
+    def forward(ctx, weight, N, P, M, decay = 0.0002):
 
         ctx.save_for_backward(weight)
         output = weight.clone()
@@ -46,34 +51,48 @@ class Sparse_NHWC(autograd.Function):
 
         weight_temp = weight.detach().abs().permute(0,2,3,1).reshape(group, M)
         index = torch.argsort(weight_temp, dim=1)[:, :int(M-N)]
+        F_indexes= index[:, :int(M-P)]
 
-        w_b = torch.ones(weight_temp.shape, device=weight_temp.device)
-        w_b = w_b.scatter_(dim=1, index=index, value=0).reshape(weight.permute(0,2,3,1).shape)
-        w_b = w_b.permute(0,3,1,2)
+        w_f = torch.ones(weight_temp.shape, device=weight_temp.device)
+        w_f = w_f.scatter_(dim=1, index=F_indexes, value=0).reshape(weight.permute(0,2,3,1).shape)
+        w_f = w_f.permute(0,3,1,2)
 
-        ctx.mask = w_b
         ctx.decay = decay
 
-        return output*w_b
+        if P > 0:
+            P2_indexes= index[:, int(M-P):]
+            w_p = torch.ones(weight_temp.shape, device=weight_temp.device)
+            w_p = w_p.scatter_(dim=1, index=P2_indexes, value=0).reshape(weight.permute(0,2,3,1).shape)
+            w_p = w_p.permute(0,3,1,2)
+
+            ctx.mask = w_f + w_p
+            tmp1 = output*w_f
+            tmp2 = round_to_power_of_2(output*w_p)
+            w_out = tmp1 + tmp2
+        else:
+            ctx.mask = w_f
+            w_out = output*w_f
+
+        return w_out
 
     @staticmethod
     def backward(ctx, grad_output):
-
         weight, = ctx.saved_tensors
         return grad_output + ctx.decay * (1-ctx.mask) * weight, None, None
 
 class SparseConv(nn.Conv2d):
     """" implement N:M sparse convolution layer """
     
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', N=2, M=4, **kwargs):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', N=2, P =0, M=4, **kwargs):
         self.N = N
         self.M = M
+        self.P = P
         super(SparseConv, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, padding_mode, **kwargs)
 
 
     def get_sparse_weights(self):
 
-        return Sparse_NHWC.apply(self.weight, self.N, self.M)
+        return Sparse_NHWC.apply(self.weight, self.N, self.P, self.M)
 
 
 
@@ -89,15 +108,16 @@ class SparseConv(nn.Conv2d):
 
 class SparseLinear(nn.Linear):
 
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, N=2, M=2, decay = 0.0002, **kwargs):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, N=2, P=0, M=2, decay = 0.0002, **kwargs):
         self.N = N
         self.M = M
+        self.P = P
         super(SparseLinear, self).__init__(in_features, out_features, bias = True)
 
 
     def get_sparse_weights(self):
 
-        return Sparse.apply(self.weight, self.N, self.M)
+        return Sparse.apply(self.weight, self.N, self.P, self.M)
 
 
 
